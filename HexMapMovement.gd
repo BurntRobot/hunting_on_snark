3c18070ea974e1b55235ddbd900eb541c79109e3
Burrex <Bydan14@yandex.ru>
2020-01-23 16:50:19 +0300

    Found a key to detect hex distance.

--- HexMapMovement.gd
+++ HexMapMovement.gd
@@ -8,10 +8,13 @@ var CENTER_OF_HEX = Vector2(12, 16)
 
 func _process(delta):
 	var _mouse_pos = get_local_mouse_position()
-	var _tile_pos = tiles.map_to_world(tiles.world_to_map(_mouse_pos)) + CENTER_OF_HEX
+	var _tile_pos = tiles.map_to_world(tiles.world_to_map(_mouse_pos))
+	print(tiles.world_to_map(character.position), tiles.world_to_map(_mouse_pos))  # Ключ для определения длины линии
+	_tile_pos += CENTER_OF_HEX
 	movement_line.points = PoolVector2Array([])
 	if character.position != _tile_pos:
 		var _path = PoolVector2Array([character.position, _tile_pos])
 		movement_line.points = _path
 		if Input.is_action_just_pressed("move"):
 			character.position = _tile_pos
+		print(tiles.world_to_map(character.position).distance_to(tiles.world_to_map(_mouse_pos)))
